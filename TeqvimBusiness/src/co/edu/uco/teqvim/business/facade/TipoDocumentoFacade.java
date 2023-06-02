7a015d76542645f514672e6ad82847786d75a49e
EstebanQuintero19 <111015349+EstebanQuintero19@users.noreply.github.com>
2023-06-02 13:52:59 -0500

    Delete TeqvimBusiness directory

--- TeqvimBusiness/src/co/edu/uco/teqvim/business/facade/TipoDocumentoFacade.java
+++
@@ -1,9 +0,0 @@
-package co.edu.uco.teqvim.business.facade;
-
-import java.util.List;
-import co.edu.uco.teqvim.dto.TipoDocumentoDTO;
-
-public interface TipoDocumentoFacade {
-
-	List<TipoDocumentoDTO> list(TipoDocumentoDTO dto);
-}
